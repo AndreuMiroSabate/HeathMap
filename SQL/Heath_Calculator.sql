027e3b2c1d6abe207bae8c0a6722d920a85a24b2
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2024-12-18 11:35:52 +0100

    ACTUALITZACIÓ

--- SQL/Heath_Calculator.sql
+++ SQL/Heath_Calculator.sql
@@ -1,3 +1,4 @@
-SELECT round(Z) *1000 + round(X) as HeathID,round(Z), round(X), count(*) as Heath 
+SELECT round(Z+(select ABS(min(round(Z))+1) from player_positions)) *1000 + round(X+(select ABS(min(round(X))+1) from player_positions)) as HeathID,
+round(X) AS X, round(Z) AS Z, count(*) as Heath 
 FROM andreums.player_positions
 group by 1
